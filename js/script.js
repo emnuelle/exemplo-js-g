4dee79c8866f0beb5998732639d780844da8260a
emnuelle <rm97973@fiap.com.br>
2023-03-07 11:49:12 -0300

    script.js adc ao html.

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// este é um comentário de linha 
+
+/**
+ * É um comentário 
+ * de
+ * bloco
+ */
+
+//exemplo das funções do console
+console.log("olá mundo!!")
+console.error("Bom dia!!")
+console.warn("será?")
+console.info("vai ser...")
